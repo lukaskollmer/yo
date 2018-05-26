aed3a7faf14eec72de124e3e6f42113149e95d98
Lukas Kollmer <lukas.kollmer@gmail.com>
2018-05-27 00:17:15 +0200

    moved `Array<Instruction>` to a separate file
    arrays coming soon

---
+++ yo/Bytecode Compiler/Instruction+Debug.swift
@@ -0,0 +1,45 @@
+//
+//  Instruction+Debug.swift
+//  yo
+//
+//  Created by Lukas Kollmer on 26.05.18.
+//  Copyright © 2018 Lukas Kollmer. All rights reserved.
+//
+
+import Foundation
+
+
+
+// String + padding
+extension String {
+    
+    enum PadDirection {
+        case left, right
+    }
+    
+    func padding<T: StringProtocol>(_ padDirection: PadDirection, toLength length: Int, withPad pad: T) -> String {
+        switch padDirection {
+        case .right:
+            return self.padding(toLength: length, withPad: pad, startingAt: 0)
+        case .left:
+            return String.init(repeating: pad as! String, count: length - self.count) + self
+        }
+    }
+}
+
+
+extension Array where Element == Instruction {
+    var fancyDescription: String {
+        var desc = [String]()
+        
+        for (idx, element) in self.enumerated() {
+            let instruction = InstructionDescriptor(instruction: element)
+            
+            let lineNumber = String(describing: idx).padding(.left, toLength: 3, withPad: "0")
+            let operation = String(describing: instruction.operation).padding(.right, toLength: 7, withPad: " ")
+            desc.append("  [\(lineNumber)] \(operation) \(instruction.immediate)")
+        }
+        
+        return desc.joined(separator: "\n")
+    }
+}
