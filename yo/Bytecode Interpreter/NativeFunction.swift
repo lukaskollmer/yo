18e26a3c70b4efc2d4f4be5ba6b709eff7e2edc5
Lukas Kollmer <lukas.kollmer@gmail.com>
2018-05-29 15:30:38 +0200

    [runtime] stuff

--- yo/Bytecode Interpreter/NativeFunction.swift
+++ yo/Bytecode Interpreter/NativeFunction.swift
@@ -21,46 +21,71 @@ extension Runtime {
         return SymbolMangling.mangleStaticMember(ofType: ns, memberName: name)
     }
     
-    typealias NativeFunctionImp = (StackView<Int>) -> ()
-    typealias NativeFunction = (argc: Int, address: Int, imp: NativeFunctionImp)
+    typealias NativeFunctionImp = (StackView) -> Int
+    typealias NativeFunction = (name: String, argc: Int, address: Int, imp: NativeFunctionImp)
     
-    static let builtins: [String: NativeFunction] = [
-        ns("runtime", "alloc")  : (1, addr, alloc  ),
-        ns("runtime", "retain") : (1, addr, retain ),
-        ns("runtime", "release"): (1, addr, release),
-        ns("io", "print")       : (1, addr, printf), // TODO make this variardic?
+    static let builtins: [NativeFunction] = [
+        (ns("runtime", "alloc"),            1, addr, alloc  ),
+        (ns("runtime", "retain"),           1, addr, retain ),
+        (ns("runtime", "_release"),          1, addr, runtime__release),
+        (ns("runtime", "free"),             1, addr, runtime_free),
+        (ns("runtime", "_getRetainCount"),  1, addr, getRetainCount),
+        (ns("runtime", "dealloc"),          1, addr, { _ in 0 }),   // implemented manually in -[BytecodeInterpreter run]
+        (ns("io", "print"),                 1, addr, io_printf),    // TODO make this variardic?
+        (ns("io", "printi"),                1, addr, io_printi),
     ]
     
     
     static func getNativeFunction(withAddress address: Int) -> NativeFunction {
-        if let builtin = builtins.first(where: { $0.value.address == address }) {
-            return builtin.value
+        if let builtin = builtins.first(where: { $0.address == address }) {
+            return builtin
         }
         fatalError("no native func at address \(address)")
     }
     
     
+    static func builtin(withName name: String) -> NativeFunction? {
+        return builtins.first { $0.name == name }
+    }
+    
+    
     // MARK: Native functions
     
-    private static func alloc(_ stack: StackView<Int>) {
+    private static func alloc(_ stack: StackView) -> Int {
         let size = stack.peek()
         let address = stack.heap.alloc(size: size)
         //stack.heap.retain(address: address)
-        try! stack.push(address)
+        return address
     }
     
-    private static func retain(_ stack: StackView<Int>) {
+    
+    // MARK: Reference Counting
+    
+    private static func getRetainCount(_ stack: StackView) -> Int {
+        return stack.heap.retainCount(ofAddress: stack.peek())
+    }
+    
+    private static func retain(_ stack: StackView) -> Int {
         stack.heap.retain(address: stack.peek())
-        try! stack.push(0)
+        return 0
     }
     
-    private static func release(_ stack: StackView<Int>) {
+    private static func runtime__release(_ stack: StackView) -> Int {
         stack.heap.release(address: stack.peek())
-        try! stack.push(0)
+        return 0
     }
     
-    private static func printf(_ stack: StackView<Int>) {
-        let address = stack.heap[stack.peek()]
+    private static func runtime_free(_ stack: StackView) -> Int {
+        stack.heap.free(address: stack.peek())
+        return 0;
+    }
+    
+    
+    // MARK: io
+    
+    // print a string
+    private static func io_printf(_ stack: StackView) -> Int {
+        let address = stack.heap[stack.peek() + 1]
         let size = stack.heap[address]
         
         let start = address + 1
@@ -71,6 +96,12 @@ extension Runtime {
             .map(Character.init)
         
         print(String(characters))
-        try! stack.push(0)
+        return 0
+    }
+    
+    // print an integer
+    private static func io_printi(_ stack: StackView) -> Int {
+        print(stack.peek())
+        return 0;
     }
 }
