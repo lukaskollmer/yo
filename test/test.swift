a206bb77a656c677ac4ec3045787746d9789d1d8
Lukas Kollmer <lukas.kollmer@gmail.com>
2018-05-30 20:11:39 +0200

    added a test target

---
+++ test/test.swift
@@ -0,0 +1,23 @@
+//
+//  test.swift
+//  test
+//
+//  Created by Lukas Kollmer on 30.05.18.
+//  Copyright © 2018 Lukas Kollmer. All rights reserved.
+//
+
+import XCTest
+
+class test: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}
