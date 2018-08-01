658c3e89af11c7aaced84938dc1f1120b74c524b
Lukas Kollmer <lukas.kollmer@gmail.com>
2018-08-01 11:29:11 +0200

    update FFI.swift

--- yo/libffiSwift/FFI.swift
+++ yo/libffiSwift/FFI.swift
@@ -12,33 +12,32 @@ import Foundation
 // TODO all the pointer stuff is most likely wrong
 
 
-
-typealias LKFFIFunctionPointerType = @convention(c) () -> Void
-
 class FFIFunctionInvocation {
+    typealias FunctionPointerType = @convention(c) () -> Void
     static private let _defaultHandle = dlopen(nil, RTLD_LAZY)
     
-    let functionPointer: LKFFIFunctionPointerType
+    let functionPointer: FunctionPointerType
     
     let returnType: FFIType
     let parameterTypes: [FFIType]
     
     private var cif = ffi_cif()
     
-    private var argTypes: UnsafeMutablePointer<UnsafeMutablePointer<ffi_type>?>
+    private var argTypes:  UnsafeMutablePointer<UnsafeMutablePointer<ffi_type>?>
     private var arguments: UnsafeMutablePointer<UnsafeMutableRawPointer?>
     
     init(symbol: String, handle: UnsafeMutableRawPointer? = nil, returnType: FFIType, parameterTypes: [FFIType]) {
-        self.functionPointer = unsafeBitCast(dlsym(handle ?? FFIFunctionInvocation._defaultHandle, symbol), to: LKFFIFunctionPointerType.self)
+        self.functionPointer = unsafeBitCast(dlsym(handle ?? FFIFunctionInvocation._defaultHandle, symbol), to: FunctionPointerType.self)
         
         self.returnType = returnType
         self.parameterTypes = parameterTypes
-        self.arguments = UnsafeMutablePointer<UnsafeMutableRawPointer?>.allocate(capacity: parameterTypes.count)
         
-        self.argTypes = UnsafeMutablePointer.allocate(capacity: parameterTypes.count)
+        arguments = .allocate(capacity: parameterTypes.count)
+        argTypes = .allocate(capacity: parameterTypes.count)
+        
         for (offset, type) in parameterTypes.enumerated() {
-            self.argTypes.advanced(by: offset).pointee = UnsafeMutablePointer<ffi_type>.allocate(capacity: 1)
-            self.argTypes.advanced(by: offset).pointee?.assign(from: type._pointer, count: 1)
+            argTypes.advanced(by: offset).pointee = .allocate(capacity: 1)
+            argTypes.advanced(by: offset).pointee?.assign(from: type._pointer, count: 1)
         }
         
         let status = ffi_prep_cif(&cif, FFI_DEFAULT_ABI, UInt32(parameterTypes.count), returnType._pointer, argTypes)
@@ -69,15 +68,26 @@ class FFIFunctionInvocation {
 class FFIType {
     let _pointer: UnsafeMutablePointer<ffi_type>
     
-    static let pointer = FFIType(&ffi_type_pointer)
-    static let int32   = FFIType(&ffi_type_sint32)
-    static let uint32  = FFIType(&ffi_type_uint32)
-    static let int64   = FFIType(&ffi_type_sint64)
-    static let uint64  = FFIType(&ffi_type_uint64)
-    // TODO add all other types
-    
     init(_ pointer: UnsafeMutablePointer<ffi_type>) {
         self._pointer = pointer
     }
 }
 
+extension FFIType {
+    static let void               = FFIType(&ffi_type_void)
+    static let uint8              = FFIType(&ffi_type_uint8)
+    static let int8               = FFIType(&ffi_type_sint8)
+    static let uint16             = FFIType(&ffi_type_uint16)
+    static let int16              = FFIType(&ffi_type_sint16)
+    static let uint32             = FFIType(&ffi_type_uint32)
+    static let int32              = FFIType(&ffi_type_sint32)
+    static let uint64             = FFIType(&ffi_type_uint64)
+    static let int64              = FFIType(&ffi_type_sint64)
+    static let float              = FFIType(&ffi_type_float)
+    static let double             = FFIType(&ffi_type_double)
+    static let pointer            = FFIType(&ffi_type_pointer)
+    static let complex_float      = FFIType(&ffi_type_complex_float)
+    static let complex_double     = FFIType(&ffi_type_complex_double)
+    static let complex_longdouble = FFIType(&ffi_type_complex_longdouble)
+    static let longdouble         = FFIType(&ffi_type_longdouble)
+}
