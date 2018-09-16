1217efb07eccb53e23cbc772f0d8410232c87efb
Lukas Kollmer <lukas.kollmer@gmail.com>
2018-09-16 15:49:26 +0200

    ugh

--- yo/Parser/LKYOParser.m
+++ yo/Parser/LKYOParser.m
@@ -123,7 +123,7 @@ static BOOL shouldEmitSignposts;
               expr_list, subscript, var_access, static_target, call_target, fn_call, array_literal,
               boxed_expr, range, binop_mul, binop_add, unary, lambda, typecast, lexpr, expr,
               ret, var_decl, in_place_binop, assignment, stmt_fn_call,
-              comp_op, bin_cond_op, bin_cond, comp, lcond, cond, if_stmt, else_if_stmt, else_stmt, while_stmts, for_loop_target, for_loop,
+              comp_op, bin_cond_op, bin_cond, comp, lcond, cond, if_stmt, else_if_stmt, else_stmt, while_stmt, for_loop_target, for_loop,
               asm_stmt, defer_block, stmt, composite, paramList,
               import, protocol, enum_decl, function_signature, annotation, function, topLevelStatement, program, NULL);
     
