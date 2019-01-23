2cb97323d50ebae506e922312eba3251a2b82901
Lukas Kollmer <lukas.kollmer@gmail.com>
2019-01-23 12:06:29 +0100

    [grammar] update indentation

--- yo/Parser/grammar.h
+++ yo/Parser/grammar.h
@@ -13,8 +13,8 @@
 
 
 const char *YO_GRAMMAR =
-" ident         : /[a-zA-Z_][a-zA-Z0-9_]*/;  "
-" boolean       : (\"true\" | \"false\") ;"
+" ident         : /[a-zA-Z_][a-zA-Z0-9_]*/ ;  "
+" boolean       : (\"true\" | \"false\") ;    "
 
 " number        : <number_double> "
 "               | '-'? (<number_b02> | <number_b08> | <number_b16> | <number_b10>) ;  "
@@ -22,114 +22,114 @@ const char *YO_GRAMMAR =
 
 " character     : /'[ -~]'/ ; "
 //" character     : \"[\x00-\x7F]\" ; "
-" string        : /\"[^\"\\\\]*(\\\\.[^\"\\\\]*)*\"/ ;                   "
+" string        : /\"[^\"\\\\]*(\\\\.[^\"\\\\]*)*\"/ ;  "
 // [string] we have to include the quotes as part of the regex to peoperly capture whitespace at the beginning/end of the string literal
 
 
 // Number literals
-" number_double : /[0-9]*\\.[0-9+]/ ; "
-" number_b02    : /0b[01][01_]*/ ; "
-" number_b08    : /0[0-7][0-7_]*/ ; "
-" number_b10    : /[0-9][0-9_]*/ ; "
-" number_b16    : /0x[0-9a-f][0-9a-f_]*/ ; " // TODO also allow uppercase letters? (either everything upper- or everything lowercased!)
+" number_double : /[0-9]*\\.[0-9+]/ ;       "
+" number_b02    : /0b[01][01_]*/    ;       "
+" number_b08    : /0[0-7][0-7_]*/   ;       "
+" number_b10    : /[0-9][0-9_]*/    ;       "
+" number_b16    : /0x[0-9a-f][0-9a-f_]*/ ;  " // TODO also allow uppercase letters? (either everything upper- or everything lowercased!)
 
 " fn_ptr        : \"fn\" '<' '(' (<type> (',' <type>)* )? ')' ':' <type> '>' ; "
 
-" global_var    : \"static\" <ident> ':' <type>  ('=' <expr>)?   ';'   ; "  // TODO remove the type requirement?
+" global_var    : \"static\" <ident> ':' <type> ('=' <expr>)?  ';' ; "  // TODO remove the type requirement?
 
-" struct_decl   : <annotation>* \"struct\" <ident> '{' <paramList>? '}' ';'   ;        "
+" struct_decl   : <annotation>* \"struct\" <ident> '{' <paramList>? '}' ';' ; "
 
 
-" impl          : \"impl\" <ident> (':' <ident> (',' <ident>)* )? '{' <function>* '}' ;             "
+" impl          : \"impl\" <ident> (':' <ident> (',' <ident>)* )? '{' <function>* '}' ;  "
 
-" type          : <fn_ptr>                                                          "
-"               | \"ref\" <type>                                                          "
-"               |  <ident>    ;                                                      "
+" type          : <fn_ptr>          "
+"               | \"ref\" <type>    "
+"               |  <ident>  ;       "
 
-" expr_list     : <expr> ( ',' <expr> )*  ;          "
+" expr_list     : <expr> ( ',' <expr> )*  ;     "
 
-" subscript     : <var_access> '[' <expr> ']' ;  "
+" subscript     : <var_access> '[' <expr> ']' ; "
 
-" var_access    : <ident> ( '.' <ident> )* ;      "
+" var_access    : <ident> ( '.' <ident> )* ;    "
 
-" static_target : <ident> \"::\" <ident> ; "
+" static_target : <ident> \"::\" <ident> ;      "
 
 " call_target   : ( <static_target> | <var_access> ) ;  "
 
 // TODO allow `Foo::bar().x` et al (`Foo::bar().x().y.z.a()`, etc)
-" fn_call       : <call_target> '(' <expr_list>? ')'    ;    "
+" fn_call       : <call_target> '(' <expr_list>? ')' ;  "
 
 " array_literal : ('[' ']') | ('{' '}') "
-"               | '[' <expr_list> ']' "
+"               | '[' <expr_list> ']'   "
 "               | '{' <expr_list> '}' ; "
 
-" boxed_expr    : '@' <expr> ;" // TODO use lexpr instead to have "stronger" binding? everything else would require parentheses
+" boxed_expr    : '@' <expr> ; " // TODO use lexpr instead to have "stronger" binding? everything else would require parentheses
 
 " range         : <lexpr> (\"...\" | \"..<\") <lexpr> ; "
 
 
-" binop_mul     : <lexpr> (( '*' | '/' | '%' ) (<binop_mul> | <lexpr>))*  ;   "
+" binop_mul     : <lexpr> (( '*' | '/' | '%' ) (<binop_mul> | <lexpr>))* ; "
 
 " binop_add     : <binop_mul> (( '+' | '-' | '&' | '|' | \"<<\" | \">>\" | '^') <binop_add>)* ; "
 
-" unary         : ('-' | '~' | '!') <expr>  ;  "
+" unary         : ('-' | '~' | '!') <expr> ; "
 
 
 // TODO is the second one faster?
-" lambda        : '|' (<ident> (':' <type>)? (',' <ident> (':' <type>)?)*)? '|' \"->\"  <composite>      ;           "
+" lambda        : '|' (<ident> (':' <type>)? (',' <ident> (':' <type>)?)*)? '|' \"->\" <composite> ; "
 //" lambda        : (('|' '|') | ('|' <ident> (':' <type>)? (',' <ident> (':' <type>)?)*) '|') \"->\" <composite> ;  "
 
-" typecast      : <lexpr> \"as\" <type>   ;  "
+" typecast      : <lexpr> \"as\" <type> ;  "
 
 
-" lexpr         : <boolean>                             "
-"               | <lambda>  "
-"               | '(' <expr>  ')'                       "
-"               | <number>                       "
-"               | <unary>                       "
-"               | <character>                       "
-"               | <string>                       "
-"               | <array_literal>                       "
+" lexpr         : <boolean>         "
+"               | <lambda>          "
+"               | '(' <expr>  ')'   "
+"               | <number>          "
+"               | <unary>           "
+"               | <character>       "
+"               | <string>          "
+"               | <array_literal>   "
 // TODO can/should <fn_call> be moved to <expr> ?
-"               | <fn_call>                       "
-"               | <subscript>                       "
-"               | <var_access>     ;                  "
+"               | <fn_call>         "
+"               | <subscript>       "
+"               | <var_access> ;    "
 // TODO: potential improvement: parse <var_access> once, then optionally subscript access or a function call
 // This would break static function calls, which could be fixed by replacing the currently freestanding <static_target> option w/ `<static_target> ('(' <expr_list> ')' )?` TODO!!! this really shouldn't be too difficult
 //"               | <var_access> ( ('[' <expr> ']') | ('('<expr_list>?')') )?    "
 
 
-" expr          : <typecast>                    "
-"               | <range> "
-"               | <binop_add>               "
-"               | <binop_mul>                                                           "
-"               | <boxed_expr>                                                           "
-"               | \"nil\" ;                                                         "
+" expr          : <typecast>    "
+"               | <range>       "
+"               | <binop_add>   "
+"               | <binop_mul>   "
+"               | <boxed_expr>  "
+"               | \"nil\" ;     "
 
 " ret           : \"ret\" <expr>? ';' ; "
 
 " var_decl      : \"val\" <ident> (':' <type>)? ('=' <expr>)? ';' ; "
 
 // TODO split up the various kinds of assignments? (attribute, subscript, etc)
-" in_place_binop    : (\"+=\" | \"-=\" | \"*=\" | \"/=\" | \"%=\" | \"|=\" | \"&=\" | \"^=\" | \">>=\" | \"<<=\")  ;    "
-" assignment    : <var_access> ( '[' <expr> ']' )? ('=' | <in_place_binop> ) <expr>  ';' ;             "
+" in_place_binop    : (\"+=\" | \"-=\" | \"*=\" | \"/=\" | \"%=\" | \"|=\" | \"&=\" | \"^=\" | \">>=\" | \"<<=\") ; "
+" assignment    : <var_access> ( '[' <expr> ']' )? ('=' | <in_place_binop> ) <expr>  ';' ;  "
 
 // Q: Why is stmt_fn_call its own thing, instead of replacing the respective line in stmt with `<fn_call> ';'` ?
 // A: We have to make sure the semicolon is part of the statement function call, which is important to have them both put in the same ast node
-" stmt_fn_call  : <fn_call> ';'  ; "
+" stmt_fn_call  : <fn_call> ';' ;   "
 
 
-" comp_op       : \"==\" | \"!=\" | \"<=\" | \">=\" | '<' | '>'  ; "
-" bin_cond_op   : \"&&\" | \"||\" ;  "
+" comp_op       : \"==\" | \"!=\" | \"<=\" | \">=\" | '<' | '>' ; "
+" bin_cond_op   : \"&&\" | \"||\" ; "
 
-" bin_cond      : <lcond> <bin_cond_op> <cond>  ; "
+" bin_cond      : <lcond> <bin_cond_op> <cond> ; "
 
-" comp          : <expr> <comp_op> <expr>   ; " // TODO expr or lexpr?
+" comp          : <expr> <comp_op> <expr> ; " // TODO expr or lexpr?
 
 // a part of a condition that can evaluate to true
 //" lcond         : '(' <cond> ')'  " // TODO this breaks expressions wrapped in parentheses (`if (7-1) >= 5 ...`)
-" lcond         : <comp>  "
-"               | <expr>  ; "
+" lcond         : <comp>    "
+"               | <expr> ;  "
 
 //" cond          : <bin_cond>    "
 //"               | <lcond> ; "
@@ -137,40 +137,40 @@ const char *YO_GRAMMAR =
 // TODO seems like there's no difference between `*` and `?` bc rhs is <cond> which will match a new binop
 " cond          : <lcond> (<bin_cond_op> <cond>)? ; "
 
-" if_stmt       : \"if\" <cond> <composite> <else_if_stmt>* <else_stmt>?  ;   "
-" else_if_stmt  : \"else\" \"if\" <cond> <composite>  ;  "
-" else_stmt     :  \"else\" <composite>  ;  "
+" if_stmt       : \"if\" <cond> <composite> <else_if_stmt>* <else_stmt>? ;  "
+" else_if_stmt  : \"else\" \"if\" <cond> <composite> ;                      "
+" else_stmt     :  \"else\" <composite> ;                                   "
 
-" while_stmt    : \"while\" <cond> <composite>     ;         "
+" while_stmt    : \"while\" <cond> <composite> ;    "
 
-" for_loop_target   : <range>  ; "
+" for_loop_target   : <range> ; "
 
-" for_loop      : \"for\" <ident> (':' <type>)?  \"in\"  <expr>   <composite>     "
-"               | \"for\" <var_decl> <cond> ';' <stmt> <composite> ;"
+" for_loop      : \"for\" <ident> (':' <type>)? \"in\" <expr> <composite>   "
+"               | \"for\" <var_decl> <cond> ';' <stmt> <composite> ;        "
 
 
-" asm_stmt      : \"__asm\" '(' <ident> ',' (<number> | <string>) ')' ';'  ;   "
+" asm_stmt      : \"__asm\" '(' <ident> ',' (<number> | <string>) ')' ';' ; "
 
-" defer_block   : \"defer\" <composite>   ; "
+" defer_block   : \"defer\" <composite> ;   "
 
 " stmt          : <ret>             "
 "               | <composite>       "
-"               | <assignment>        "
-"               | <var_decl>     "
-"               | <asm_stmt>       "
+"               | <assignment>      "
+"               | <var_decl>        "
+"               | <asm_stmt>        "
 "               | <stmt_fn_call>    "
-"               | <if_stmt>       "
-"               | <while_stmt>       "
+"               | <if_stmt>         "
+"               | <while_stmt>      "
 "               | <for_loop>        "
-"               | <defer_block> ;     "
+"               | <defer_block> ;   "
 // var_decl after assignment because it can interfer w/ other statements
 // example: `value = 12;` would be parsed as `val ue = 12;`
 
-" composite     : \"unsafe\"? '{' <stmt>* '}' ;   "
+" composite     : \"unsafe\"? '{' <stmt>* '}' ; "
 
-" paramList     : <ident> ':' <type> (',' <ident> ':' <type> )* ; "
+" paramList     : <ident> ':' <type> (',' <ident> ':' <type> )* ;   "
 
-" import        : \"use\" <string> ';'  ;  "
+" import        : \"use\" <string> ';' ;    "
 
 " protocol      : <annotation>? \"protocol\" <ident> '{' ((<function_signature> ';') | <function>)* '}' ; "
 
@@ -184,4 +184,5 @@ const char *YO_GRAMMAR =
 
 " topLevelStatement : ( <import> | <global_var> | <protocol> | <enum_decl> | <function> | <struct_decl> | <impl> ) ; "
 
-" program       : /^/  <topLevelStatement>* /$/;               ";
+" program       : /^/  <topLevelStatement>* /$/ ;   "
+;
